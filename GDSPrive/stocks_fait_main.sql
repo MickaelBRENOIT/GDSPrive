22ce379024496ef9b4d1b2585c8c935da85e0ac7
Mickael <brenoit.mickael@gmail.com>
2017-02-16 21:31:42 +0100

    Modification du fichier sql qui prend maintenant en compte les fournisseurs.

---
+++ GDSPrive/stocks_fait_main.sql
@@ -0,0 +1,37 @@
+DROP TABLE IF EXISTS utilisateur;
+DROP TABLE IF EXISTS role;
+
+CREATE TABLE role (
+    id_role INT(11) UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    nom_role varchar(20) NOT NULL
+) ENGINE=InnoDB; 
+
+INSERT INTO `role` (`id_role`, `nom_role`) VALUES
+(1, 'administrateur'),
+(2, 'utilisateur');
+
+CREATE TABLE utilisateur (
+    id_utilisateur INT(11) UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    nom_utilisateur varchar(50) NOT NULL,
+    prenom_utilisateur varchar(50) NOT NULL,
+    email_utilisateur varchar(50) NOT NULL,
+    password_utilisateur varchar(50) NOT NULL,
+    date_naissance datetime NOT NULL,
+    date_embauche datetime NOT NULL,
+    adresse_utilisateur varchar(100) NOT NULL,
+    ce_role INT(11) UNSIGNED NOT NULL,
+    numero_utilisateur varchar(10) NOT NULL,
+    FOREIGN KEY (ce_role) REFERENCES role(id_role)
+) ENGINE=InnoDB;
+
+INSERT INTO `utilisateur` (`id_utilisateur`, `nom_utilisateur`, `prenom_utilisateur`, `email_utilisateur`, `password_utilisateur`, `date_naissance`, `date_embauche`, `adresse_utilisateur`, `ce_role`, `numero_utilisateur`) VALUES
+(1, 'BRENOIT', 'Mickael', 'mickael.brenoit@uha.fr', '0000', '1994-05-30 00:00:00', '2016-10-10 00:00:00', '6 rue de Sochaux, 68200 Mulhouse', 1, '0389010203'),
+(2, 'DUPONT', 'Pierre', 'pierre.dupont@uha.fr', '1234', '1988-02-10 00:00:00', '2016-04-19 00:00:00', '17 rue du Soleil, 67000 Strasbourg', 2, '0389070809');
+
+CREATE TABLE fournisseur (
+    id_fournisseur INT(11) UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    societe_fournisseur varchar(50),
+    adresse_fournisseur varchar(50) NOT NULL,
+    domaine_fournisseur varchar(50) NOT NULL,
+    numero_fournisseur varchar(10) NOT NULL
+) ENGINE=InnoDB;
