3d754765226ac4cd12f72058167b16977a88ecdf
Mickael <brenoit.mickael@gmail.com>
2017-02-16 22:44:22 +0100

    Ajout de la fonctionnalité qui liste les fournisseurs

--- GDSPrive/src/supplier/SupplierUserFrame.java
+++ GDSPrive/src/supplier/SupplierUserFrame.java
@@ -6,8 +6,11 @@
 package supplier;
 
 import authentication.Authentication;
+import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.List;
+import javax.swing.Box;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -20,32 +23,33 @@ import menu.UserMenu;
  *
  * @author Mikael
  */
-public class SupplierUserFrame extends JFrame implements ActionListener{
-    
+public class SupplierUserFrame extends JFrame implements ActionListener {
+
     private JButton list;
     private JButton searchButton;
     private JButton returnToPreviousFrame;
     private JButton exit;
-    
+
     private JTextField searchField;
     private JTextArea listSuppliers;
     private JScrollPane scrollSuppliers;
-    
+
     private JPanel main;
-    
+
     private Authentication authentication;
+    private SupplierDAO supplier;
 
     public SupplierUserFrame(Authentication auth) {
         authentication = auth;
         this.setTitle("Gestion des fournisseurs | " + auth.getLogin());
-        this.setSize(800, 800);
-        
+        this.setSize(800, 300);
+
         main = new JPanel();
         add(main);
         
         initialize();
         disposition();
-        
+
         setResizable(false);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -53,30 +57,34 @@ public class SupplierUserFrame extends JFrame implements ActionListener{
     }
 
     private void initialize() {
-        searchButton = new JButton("Rechercher");
-        searchButton.setBounds(550, 50, 200, 40);
+
+        this.supplier = new SupplierDAO();
+
+        searchButton = new JButton("Rechercher par domaine");
+        searchButton.setBounds(550, 50, 200, 30);
         searchButton.addActionListener(this);
-        
+
         list = new JButton("Lister");
-        list.setBounds(550, 150, 200, 40);
+        list.setBounds(550, 100, 200, 30);
         list.addActionListener(this);
-        
+
         returnToPreviousFrame = new JButton("Retour");
-        returnToPreviousFrame.setBounds(550, 250, 200, 40);
+        returnToPreviousFrame.setBounds(550, 150, 200, 30);
         returnToPreviousFrame.addActionListener(this);
-        
+
         exit = new JButton("Quitter");
-        exit.setBounds(550, 350, 200, 40);
+        exit.setBounds(550, 200, 200, 30);
         exit.addActionListener(this);
-        
+
         searchField = new JTextField("Champ de recherche");
         searchField.setBounds(50, 50, 400, 25);
-        
+
         listSuppliers = new JTextArea();
-        listSuppliers.setBounds(50, 75, 400, 100);
-        
         scrollSuppliers = new JScrollPane(listSuppliers);
-        
+        scrollSuppliers.setBounds(50, 75, 400, 155);
+        scrollSuppliers.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        listSuppliers.setEditable(false);
+
     }
 
     private void disposition() {
@@ -87,22 +95,26 @@ public class SupplierUserFrame extends JFrame implements ActionListener{
         main.add(returnToPreviousFrame);
         main.add(exit);
         main.add(searchField);
-        main.add(listSuppliers);
         main.add(scrollSuppliers);
     }
-    
+
     @Override
     public void actionPerformed(ActionEvent ae) {
-        if(ae.getSource() == searchButton){
+        if (ae.getSource() == searchButton) {
             System.out.println("Cliqué sur le bouton rechercher");
-        }else if(ae.getSource() == list){
-            System.out.println("Cliqué sur le bouton listé");
-        }else if(ae.getSource() == returnToPreviousFrame){
+        } else if (ae.getSource() == list) {
+            listSuppliers.setText(null);
+            List<Supplier> listOfSuppliers = supplier.getListSuppliers();
+            for (Supplier s : listOfSuppliers) {
+                listSuppliers.append(s.toString());
+                listSuppliers.append("\n");	
+            }
+        } else if (ae.getSource() == returnToPreviousFrame) {
             this.dispose();
             UserMenu um = new UserMenu(authentication);
-        }else if(ae.getSource() == exit){
+        } else if (ae.getSource() == exit) {
             this.dispose();
         }
     }
-    
+
 }
