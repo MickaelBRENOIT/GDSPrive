272de6304a5b3e33c98883a436fd0757ede13a2b
Mickael <brenoit.mickael@gmail.com>
2017-02-16 21:35:14 +0100

    Ajout de l'attribut authentication qui permet de pouvoir transférer les données de la personne transférée à la fenêtre suivante.

--- GDSPrive/src/menu/UserMenu.java
+++ GDSPrive/src/menu/UserMenu.java
@@ -7,6 +7,7 @@ import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import supplier.SupplierUserFrame;
 
 public class UserMenu extends JFrame implements ActionListener{
     private JButton suppliers;
@@ -14,10 +15,13 @@ public class UserMenu extends JFrame implements ActionListener{
     private JButton exit;
     private JButton signOut;
     
-    private JPanel main;
+    private final JPanel main;
+    
+    private final Authentication authentication;
 
     public UserMenu(Authentication auth) {
-        this.setTitle("Utilisateur | "+auth.getLogin());
+        authentication = auth;
+        this.setTitle("Utilisateur | "+authentication.getLogin());
         this.setSize(400, 200);
         
         main = new JPanel();
@@ -64,12 +68,13 @@ public class UserMenu extends JFrame implements ActionListener{
     public void actionPerformed(ActionEvent ae) {
         try {
             if (ae.getSource() == suppliers) {
-                System.out.println("Cliqué pour les fournisseurs");
+                this.dispose();
+                SupplierUserFrame suf = new SupplierUserFrame(authentication);
             }else if (ae.getSource() == costumers){
                 System.out.println("Cliqué pour les clients");
             }else if (ae.getSource() == signOut){
-                AuthenticationFrame af = new AuthenticationFrame();
                 this.dispose();
+                AuthenticationFrame af = new AuthenticationFrame();
             }else if (ae.getSource() == exit){
                 this.dispose();
             }
