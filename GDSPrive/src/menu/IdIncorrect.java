029dfea53123301bedd3886e1545692a72624b26
Mickael <brenoit.mickael@gmail.com>
2017-02-16 07:04:37 +0100

    Ajout d'une fonction qui lance un son lorsque la fenêtre s'affiche.

--- GDSPrive/src/menu/IdIncorrect.java
+++ GDSPrive/src/menu/IdIncorrect.java
@@ -8,6 +8,14 @@ package menu;
 import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JDialog;
@@ -18,8 +26,8 @@ import javax.swing.JPanel;
  *
  * @author e1500727
  */
-public class IdIncorrect extends JDialog implements ActionListener{
-    
+public class IdIncorrect extends JDialog implements ActionListener {
+
     private JPanel main;
     private JButton buttonOk;
     private JLabel text;
@@ -35,20 +43,22 @@ public class IdIncorrect extends JDialog implements ActionListener{
 
         initialize();
         disposition();
-        
+        playSound();
+
         setLocationRelativeTo(null);
         setResizable(false);
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
         setModal(true);
         setVisible(true);
+
     }
 
     private void initialize() {
         text = new JLabel("Le login ou le mot de passe est incorrect");
-        
+
         warningImg = new ImageIcon("images/warning.png");
         warningLabel = new JLabel("", warningImg, JLabel.CENTER);
-        
+
         buttonOk = new JButton("Ok");
         buttonOk.addActionListener(this);
     }
@@ -61,8 +71,28 @@ public class IdIncorrect extends JDialog implements ActionListener{
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if(e.getSource() == buttonOk){
+        if (e.getSource() == buttonOk) {
             this.dispose();
+        }
+    }
+
+    private void playSound() {
+        try {
+            // Open an audio input stream.
+            String songName = "windowsLoud.wav";
+            String pathToWav = System.getProperty("user.dir") +"\\sounds\\"+ songName;
+            AudioInputStream audioIn = AudioSystem.getAudioInputStream(new File(pathToWav).getAbsoluteFile());
+            // Get a sound clip resource.
+            Clip clip = AudioSystem.getClip();
+            // Open audio clip and load samples from the audio input stream.
+            clip.open(audioIn);
+            clip.start();
+        } catch (UnsupportedAudioFileException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (LineUnavailableException e) {
+            e.printStackTrace();
         }
     }
 }
