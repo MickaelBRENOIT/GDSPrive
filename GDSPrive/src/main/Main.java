c8b1bbff5ae6423156240814c402816d4f34ef5d
Mickael <brenoit.mickael@gmail.com>
2017-03-01 00:08:30 +0100

    Ajout des commentaires pour le main

--- GDSPrive/src/main/Main.java
+++ GDSPrive/src/main/Main.java
@@ -2,14 +2,14 @@ package main;
 
 import authentication.AuthenticationFrame;
 
-
 public class Main {
 
     /**
      * Permet de lancer l'application "Gestion des stocks"
+     *
      * @param args
      */
-    public static void main(String[] args){
+    public static void main(String[] args) {
         AuthenticationFrame af = new AuthenticationFrame();
     }
 }
