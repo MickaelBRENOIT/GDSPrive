c5fd9f426665b2e683fe32475d8d8776f184ec84
Mickael <brenoit.mickael@gmail.com>
2017-03-01 00:07:57 +0100

    Ajout des commentaires pour le main

--- GDSPrive/src/main/Main.java
+++ GDSPrive/src/main/Main.java
@@ -1,17 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package main;
 
 import authentication.AuthenticationFrame;
 
-/**
- *
- * @author Mikael
- */
+
 public class Main {
+
+    /**
+     * Permet de lancer l'application "Gestion des stocks"
+     * @param args
+     */
     public static void main(String[] args){
         AuthenticationFrame af = new AuthenticationFrame();
     }
